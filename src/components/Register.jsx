01b4ac8faf7981c9b4e86f7a2ad172caf8cc0ecb
Tochukwu Ewiwilem <tochukwuewiwilem@gmail.com>
2023-10-16 15:11:48 +0200

    add newUser to database

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -2,33 +2,56 @@ import FloatingLabel from 'react-bootstrap/FloatingLabel';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import { useState } from 'react';
-// import api from './api';
+import api from '../api';
 
 function Register() {
     const [firstname, setFirstname] = useState('');
     const [lastname, setLastname] = useState('');
-    const [email, setemail] = useState('');
-
+    const [email, setEmail] = useState('');
     const newUser = { firstname, lastname, email };
-    const handleSubmit = async () => {
+ 
+    const handleFirstnameInput = (event) => {
+        setFirstname(event.target.value)
+    }
+    const handleLastnameInput = (event) => {
+        setLastname(event.target.value)
+    }
+    const handleEmailInput = (event) => {
+        setEmail(event.target.value)
+    }
+    const handleSubmit = async (event) => {
+        event.preventDefault();
 
+        const registerNewUser = async () => {
+            try {
+                const response = await api.put('/users', newUser);
+                console.log(response)
+            } catch (error) {
+                console.log(error.response.message)
+            }
+            
+        }
+        registerNewUser();
     }
 
   return (
     <Form onSubmit={handleSubmit}>
         
       <FloatingLabel className="mb-2" controlId="floatingInput" label="First name">
-        <Form.Control type="text" placeholder="firstname" />
+        <Form.Control type="text" value={firstname}
+                                onChange={handleFirstnameInput} />
       </FloatingLabel>
       <FloatingLabel className="mb-2" controlId="floatingInput" label="Last name">
-        <Form.Control type="text" placeholder="lastname" />
+        <Form.Control type="text" value={lastname}
+                                onChange={handleLastnameInput} />
       </FloatingLabel>
       <FloatingLabel
         controlId="floatingInput"
         label="Email address"
         className="mb-3"
       >
-        <Form.Control type="email" placeholder="name@example.com" />
+        <Form.Control type="email" value={email}
+                                onChange={handleEmailInput} />
       </FloatingLabel>
       <Button variant="primary" type="submit">
         Submit
